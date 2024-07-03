8694cc355437f3f6c459c37fcead367ce3f06ec3
Jack <jackbadawy1999@gmail.com>
2024-07-03 16:07:43 +1000

    modal animation

--- src/app/Components/AlertModal.tsx
+++ src/app/Components/AlertModal.tsx
@@ -14,8 +14,8 @@ const Modal: React.FC<ModalProps> = ({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-      <div className="bg-slate-800 p-6 rounded-lg shadow-md text-white w-80">
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 fade-in">
+      <div className="bg-slate-800 p-6 rounded-lg shadow-md text-white w-80 slide-down">
         <h2 className="text-lg font-semibold mb-2">Confirmation</h2>
         <div className="bg-slate-700 p-1 rounded mb-4 px-2">
           <p className="mb-1">{message}</p>
