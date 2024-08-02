7feaa4e088e22d005e2c0671ab65b234211ba19e
Jack <jackbadawy1999@gmail.com>
2024-08-02 20:34:11 +1000

    counter

--- src/app/Components/SelectionCarosel.tsx
+++ src/app/Components/SelectionCarosel.tsx
@@ -11,6 +11,8 @@ const SelectionCarosel: React.FC<SelectionCaroselProps> = ({
   jobTitle,
 }) => {
   const [userInfo, setUserInfo] = useState<string[]>([]);
+  const [templateIndex, setTemplateIndex] = useState<number>(0);
+
   useEffect(() => {
     console.log("this is a carousel test");
     if (fullName && jobTitle) {
@@ -24,13 +26,54 @@ const SelectionCarosel: React.FC<SelectionCaroselProps> = ({
     //
   };
 
+  const templateCounter = () => {
+    return `${templateIndex + 1} / ${aboutMeTemplates.length}`;
+  };
+
+  const returnTemplate = (index) => {
+    return aboutMeTemplates[index];
+  };
+
+  const iterateCounter = (operator) => {
+    switch (operator) {
+      case "+":
+        setTemplateIndex(
+          templateIndex != aboutMeTemplates.length - 1 ? templateIndex + 1 : 0
+        );
+        break;
+      case "-":
+        setTemplateIndex(templateIndex != 0 ? templateIndex - 1 : 0);
+        break;
+      default:
+        setTemplateIndex(0);
+      /* throw "invalid operator passed to about me carosel"; */
+    }
+  };
+
   {
     return (
       <div>
-        <p>{userInfo[0]}</p>
-        <p>{userInfo[1]}</p>
+        {/* <p>{userInfo[0]}</p>
+        <p>{userInfo[1]}</p> */}
+        <div className="flex justify-end gap-2 mb-1">
+          <button
+            onClick={() => iterateCounter("-")}
+            className="px-4 py-2 bg-bws rounded mb-1"
+          >
+            -
+          </button>
+          <span className="bg-slate-700 p-1 rounded mb-2 px-2">
+            {templateCounter()}
+          </span>
+          <button
+            onClick={() => iterateCounter("+")}
+            className="px-4 py-2 bg-bws rounded mb-1"
+          >
+            +
+          </button>
+        </div>
         <div className="bg-slate-700 p-1 rounded mb-4 px-2 w-max">
-          <p>{aboutMeTemplates[0]}</p>
+          <p>{returnTemplate(templateIndex)}</p>
         </div>
         <button className="px-4 py-2 bg-bws rounded mt-1">Add Template</button>
       </div>
